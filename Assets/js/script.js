545c56092c6f7e32b5826ed3cfb59dd56284bbfa
Amy - Leigh Sawyer <amyleighsawyer97@gmail.com>
2023-05-08 19:08:11 +0200

    Added score function and image function to display results

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,53 +9,75 @@ document.addEventListener("DOMContentLoaded", function() {
         const computerChoice = choices[Math.floor(Math.random() * choices.length)];
         console.log(computerChoice);
         console.log(playerChoice);
-        getResult(playerChoice, computerChoice);
+        let result = getResult(playerChoice, computerChoice);
+        displayResult(playerChoice, computerChoice, result);
         });
     }
 });
 
+
+
 function getResult(playerChoice, computerChoice){
-    let result = document.getElementById('result');
     if (playerChoice === computerChoice) {
 		alert("tie");
     }else if(playerChoice == 'rock'){
         if(computerChoice == 'paper' || computerChoice == 'spock'){
             alert("Computer won");
+            incrementComputer();
         } else{
             alert("Player won");
+            incrementPlayer();
         }
     }else if(playerChoice == 'scissors'){
         if(computerChoice == 'rock' || computerChoice == 'spock'){
             alert("Computer won");
+            incrementComputer();
         } else{
             alert("Player won");
+            incrementPlayer();
         }
     }else if(playerChoice == 'paper'){
         if(computerChoice == 'scissors' || computerChoice == 'lizard'){
             alert("Computer won");
+            incrementComputer();
         } else{
             alert("Player won");
+            incrementPlayer();
         }
     }else if(playerChoice == 'lizard'){
         if(computerChoice == 'scissors' || computerChoice == 'rock'){
             alert("Computer won");
+            incrementComputer();
         } else{
             alert("Player won");
+            incrementPlayer();
         }
     }else if(playerChoice == 'spock'){
         if(computerChoice == 'paper' || computerChoice == 'lizard'){
             alert("Computer won");
+            incrementComputer();
         } else{
             alert("Player won");
+            incrementPlayer();
         }
     }
   
 }            
 
-function incrementWinner(){
-
+function incrementPlayer(){
+    let playerScore = parseInt(document.getElementById("player-score").innerText);
+    document.getElementById("player-score").innerText = ++playerScore
 }
 
-function incrementLoser(){
+function incrementComputer(){
+    let computerScore = parseInt(document.getElementById("computer-score").innerText);
+    document.getElementById("computer-score").innerText = ++computerScore
+}
 
+function displayResult(playerChoice, computerChoice) {
+    let playerImage = `<img class="display-img" src="${playerChoice}.png">`
+    let computerImage = `<img class="display-img" src="${computerChoice}.png">`
+
+    document.getElementById("player-choice").innerHTML = playerImage;
+    document.getElementById("computer-choice").innerHTML = computerImage;
 }
